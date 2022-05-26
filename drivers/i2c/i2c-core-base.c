daf1afb0dd7a562d2317a6a3e71c2cebadba410d
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2022-05-26 15:12:13 +0200

    i2c: dummy: Drop no-op remove function
    A remove callback that just returns 0 is equivalent to no callback at all
    as can be seen in i2c_device_remove(). So simplify accordingly.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Wolfram Sang <wsa@kernel.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- drivers/i2c/i2c-core-base.c
+++ drivers/i2c/i2c-core-base.c
@@ -907,16 +907,10 @@ static int dummy_probe(struct i2c_client *client,
 	return 0;
 }
 
-static int dummy_remove(struct i2c_client *client)
-{
-	return 0;
-}
-
 static struct i2c_driver dummy_driver = {
 	.driver.name	= "dummy",
 	.driver.probe_type = PROBE_FORCE_SYNCHRONOUS,
 	.probe		= dummy_probe,
-	.remove		= dummy_remove,
 	.id_table	= dummy_id,
 };
 
