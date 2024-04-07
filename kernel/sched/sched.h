4d878df9604d293289af1e5d732f35e96145f29b
Qais Yousef <qyousef@layalina.io>
2024-04-07 14:40:06 +0100

    BACKPORT: sched: cpufreq: Remove magic 1.25 headroom from apply_dvfs_headroom()
    Replace 1.25 headroom in apply_dvfs_headroom() with better dynamic
    logic.
    
    Instead of the magical 1.25 headroom, use the new approximate_util_avg()
    to provide headroom based on the dvfs_update_delay; which is the period
    at which the cpufreq governor will send DVFS updates to the hardware.
    
    Add a new percpu dvfs_update_delay that can be cheaply accessed whenever
    apply_dvfs_headroom() is called. We expect cpufreq governors that rely
    on util to drive its DVFS logic/algorithm to populate these percpu
    variables. schedutil is the only such governor at the moment.
    
    The behavior of schedutil will change as the headroom will be less than
    1.25 for most systems as the rate_limit_us is usually short.
    
    Change-Id: Ida318e957682f1d88e64b6568b15959e8dbc2b77
    Signed-off-by: Qais Yousef <qyousef@layalina.io>
    [Tashar02: s/h_nr_running/h_nr_queued/ as per commit af44842ff0 ("sched/fair: Rename h_nr_running into h_nr_queued")]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -412,6 +412,15 @@ bool __dl_overflow(struct dl_bw *dl_b, int cpus, u64 old_bw, u64 new_bw)
 unsigned long approximate_util_avg(unsigned long util, u64 delta);
 u64 approximate_runtime(unsigned long util);
 
+/*
+ * Any governor that relies on util signal to drive DVFS, must populate these
+ * percpu dvfs_update_delay variables.
+ *
+ * It should describe the rate/delay at which the governor sends DVFS freq
+ * update to the hardware in us.
+ */
+DECLARE_PER_CPU_READ_MOSTLY(u64, dvfs_update_delay);
+
 extern void dl_change_utilization(struct task_struct *p, u64 new_bw);
 extern void init_dl_bw(struct dl_bw *dl_b);
 extern int  sched_dl_global_validate(void);
