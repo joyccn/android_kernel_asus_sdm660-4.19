6e11404d0a9ad7f8aa859665e5b630054bfdd9d7
rsuntk <rissu.ntk@gmail.com>
2026-03-25 00:52:08 +0700

    msm: camera_v2: Revert previous changes
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/cci/msm_cci.c
+++ drivers/media/platform/msm/camera_v2/sensor/cci/msm_cci.c
@@ -1838,7 +1838,7 @@ static irqreturn_t msm_cci_irq(int irq_num, void *data)
 	return IRQ_HANDLED;
 }
 
-static int __maybe_unused msm_cci_irq_routine(struct v4l2_subdev *sd, u32 status,
+static int msm_cci_irq_routine(struct v4l2_subdev *sd, u32 status,
 	bool *handled)
 {
 	struct cci_device *cci_dev = v4l2_get_subdevdata(sd);
@@ -1881,7 +1881,7 @@ static long msm_cci_subdev_ioctl(struct v4l2_subdev *sd,
 
 static struct v4l2_subdev_core_ops msm_cci_subdev_core_ops = {
 	.ioctl = &msm_cci_subdev_ioctl,
-	/* .interrupt_service_routine is deprecated and not needed in 4.19 */
+	.interrupt_service_routine = msm_cci_irq_routine,
 };
 
 static const struct v4l2_subdev_ops msm_cci_subdev_ops = {
