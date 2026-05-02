b89cca89f1c3196b32c343241a360e56e587f6aa
ris <rissu.ntk@gmail.com>
2026-05-03 03:41:52 +0700

    Revert "UPSTREAM: mm/slab: refactor common ksize KASAN logic into slab_common.c"
    This reverts commit 65519ddf3196c4c311b8cba26c9470e8060c1345.
    
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- mm/slub.c
+++ mm/slub.c
@@ -4007,7 +4007,7 @@ void __check_heap_object(const void *ptr, unsigned long n, struct page *page,
 }
 #endif /* CONFIG_HARDENED_USERCOPY */
 
-size_t __ksize(const void *object)
+static size_t __ksize(const void *object)
 {
 	struct page *page;
 
@@ -4023,7 +4023,17 @@ size_t __ksize(const void *object)
 
 	return slab_ksize(page->slab_cache);
 }
-EXPORT_SYMBOL(__ksize);
+
+size_t ksize(const void *object)
+{
+	size_t size = __ksize(object);
+	/* We assume that ksize callers could use whole allocated area,
+	 * so we need to unpoison this area.
+	 */
+	kasan_unpoison_shadow(object, size);
+	return size;
+}
+EXPORT_SYMBOL(ksize);
 
 void kfree(const void *x)
 {
