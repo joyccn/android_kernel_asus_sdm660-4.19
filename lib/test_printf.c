2df7329a9167a5b962cb5fa7b925bdc0f00c95e0
ris <rissu.ntk@gmail.com>
2026-05-03 03:41:29 +0700

    Revert "UPSTREAM: lib: use KSTM_MODULE_GLOBALS macro in kselftest drivers"
    This reverts commit 9e30e718b94211cd1235c680d723b8495ffac95b.
    
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- lib/test_printf.c
+++ lib/test_printf.c
@@ -24,8 +24,8 @@
 #define PAD_SIZE 16
 #define FILL_CHAR '$'
 
-KSTM_MODULE_GLOBALS();
-
+static unsigned total_tests __initdata;
+static unsigned failed_tests __initdata;
 static char *test_buffer __initdata;
 static char *alloced_buffer __initdata;
 
